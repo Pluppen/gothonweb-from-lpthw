7610b3546d08c22a06ba843a8d3f8c9bcbe395a7
CaptenFerretE <eliass.juremalm@gmail.com>
2018-10-03 18:02:31 +0200

    Add dummy file

--- dummy.py
+++ dummy.py
@@ -18,7 +18,7 @@ session.add(user)
 user = User("jumpiness","python")
 session.add(user)
  
-# commit the record the database
+# commit the record the database #
 session.commit()
  
 session.commit()
